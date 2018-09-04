1f26f6db7a7e8782fce38d2bc17ee0f00817b951
Ian G <yo.ian.g@gmail.com>
2018-09-04 16:44:23 -0400

    Dynamic Framework support

---
+++ Framework Support/GeoJSON.h
@@ -0,0 +1,18 @@
+//
+//  GeoJSON.h
+//  GeoJSON
+//
+//  Created by Ian Grossberg on 9/4/18.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GeoJSON.
+FOUNDATION_EXPORT double GeoJSONVersionNumber;
+
+//! Project version string for GeoJSON.
+FOUNDATION_EXPORT const unsigned char GeoJSONVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GeoJSON/PublicHeader.h>
+
+
